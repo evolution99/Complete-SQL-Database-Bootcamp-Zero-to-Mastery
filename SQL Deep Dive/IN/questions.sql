aafd6b7dd0f0e17f244dd5ba9fab0a75e0123a9f
EDDISON HAYDEN LEWIS <dynamic2099@yahoo.com>
2020-12-29 21:10:54 -0400

    Update questions.sql

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -5,7 +5,7 @@
 */
 
 SELECT * FROM orders;
-
+WHERE CUSTOMER IN( 7888,1082,12808,9263)
 
 /*
 * DB: World
@@ -14,3 +14,4 @@ SELECT * FROM orders;
 */
 
 SELECT * FROM city;
+WHERE CITIES IN( Zuid-Holland, Nord-Brabant, Utrecht)
